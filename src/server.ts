e85942e633186bd76f9250be5d7177a59965a3b2
gustavoisaacc <gustavochaile7@gmail.com>
2024-10-24 06:49:17 -0300

    config cors

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+
 import { connectDB } from "./config/db";
 import { initialStatCategory } from "./utils";
 import { routeAppointment } from "./routes/appointment.routes";
 import { routeCategory } from "./routes/category.routes";
+import { configCors } from "./config/cors";
 
 connectDB();
 initialStatCategory();
 dotenv.config();
 
 export const app = express();
+app.use(cors(configCors));
+
+app.use(morgan("dev"));
+
+//leer datos del formulario
 app.use(express.json());
 
 // Import routes
